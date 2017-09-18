4d17e1ae9349810069c928d708f64fbd5046422b
Michael Morisy <m@morisy.com>
2017-09-18 10:42:43 -0400

    Update simple_parser.py

--- simple_parser.py
+++ simple_parser.py
@@ -16,8 +16,8 @@ while csvNumber < 5:
         print "Opening Crest Lite " + (str(csvNumber))
         reader = unicodecsv.DictReader(csvfile)
         for row in reader:
-            if row['document_publication_date'] != '':
-                datetime_object = datetime.strptime(row['document_publication_date'], '%B %d, %Y')
+            if row['publication_date'] != '':
+                datetime_object = datetime.strptime(row['publication_date'], '%B %d, %Y')
                 csv_file = open(str(datetime_object.strftime('%m')) + '-' + str(datetime_object.strftime('%d')) + '.csv', 'a')
                 csv_file_row_checker = csv_file
     #            try:
@@ -27,11 +27,11 @@ while csvNumber < 5:
     #                updates_so_far = 0
                 csv_writer = unicodecsv.writer(csv_file)
                 if row['title'] != "(UNTITLED)" and row['title'] != "(SANITIZED)" and row['title'] != "(Classified)":
-                    csv_writer.writerow([row['title'][0:115] + " " + row['url']])
+                    csv_writer.writerow(["OTD in 19" + str(datetime_object.strftime('%y')) + ": " + row['title'][0:100] + " " + row['url'] + " (" + row['document_page_count']+ " pgs)"])
     #            elif row['more1_title'] != '': # Double check this actually works to check if there's no more title
     #                csv_writer.writerow([row['more1_title'][0:115] + " " + row['url']])
                 else:
-                    csv_writer.writerow(["The mysterious file known only as " + row['document_number'][0:20] + " " + row['url']])
+                    csv_writer.writerow(["OTD in 19" + str(datetime_object.strftime('%y')) + ": " + "A mysterious " + row['content_type'] + " with document number  " + row['document_number'][0:20] + " " + row['url'] + " (" + row['document_page_count']+ " pgs)"])
             else:
                 print "No date"
 
