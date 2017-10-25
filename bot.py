a644c98f4688c586113b44a65b3f99a81de01109
Michael Morisy <m@morisy.com>
2017-10-24 22:57:56 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,30 +1,39 @@
-from twython import Twython
-import os
-from datetime import date, datetime
-import html
-from db import RPiBlogDatabase
+import unicodecsv
+from datetime import datetime
+from difflib import SequenceMatcher
 
-twitter = Twython(
-    os.environ['RPOTD_CON_KEY'],
-    os.environ['RPOTD_CON_SEC'],
-    os.environ['RPOTD_ACC_TOK'],
-    os.environ['RPOTD_ACC_SEC']
-)
+# To do:
+# - Limit to X updates per day
+# - Post tweets!
+# - Screenshot
+# - ???
 
-db = RPiBlogDatabase()
+csvNumber = 1
+deduplicate = True
 
-now = datetime.now()
-month = now.date().month
-day = now.date().day
-year = 2002 + now.time().hour  # 9am = 2011
-post_date = date(year, month, day)
-post = db.get_post_by_date(post_date)
+while csvNumber < 5:
+    with open('crest_lite_' + str(csvNumber) + '.csv') as csvfile:
+        print "Opening Crest Lite " + (str(csvNumber))
+        reader = unicodecsv.DictReader(csvfile)
+        for row in reader:
+            if row['publication_date'] != '':
+                datetime_object = datetime.strptime(row['publication_date'], '%B %d, %Y')
+                    csv_file = open(str(datetime_object.strftime('%m')) + '-' + str(datetime_object.strftime('%d')) + '.csv', 'a')
+                csv_file_row_checker = csv_file
+    #            try:
+    #                updates_so_far = len(list(csv_file))
+    #                print str(updates_so_far)
+    #            except:
+    #                updates_so_far = 0
+                csv_writer = unicodecsv.writer(csv_file)
+                if row['title'] != "(UNTITLED)" and row['title'] != "(SANITIZED)" and row['title'] != "(Classified)":
+                    csv_writer.writerow(["OTD in 19" + str(datetime_object.strftime('%y')) + ": " + row['title'][0:100] + " " + row['url'] + " (" + row['document_page_count']+ " pgs)"])
+    #            elif row['more1_title'] != '': # Double check this actually works to check if there's no more title
+    #                csv_writer.writerow([row['more1_title'][0:115] + " " + row['url']])
+                else:
+                    csv_writer.writerow(["OTD in 19" + str(datetime_object.strftime('%y')) + ": " + "A mysterious " + row['content_type'] + " with document number  " + row['document_number'][0:20] + " " + row['url'] + " (" + row['document_page_count']+ " pgs)"])
+            else:
+                print "No date"
 
-if post:
-    title = html.unescape(post['title'])
-    slug = post['slug']
-    url = 'https://www.raspberrypi.org/blog/{}'.format(slug)
-
-    tweet = "On this day in {}: {} {}".format(year, title, url)
-    print('Tweeting: {}'.format(tweet))
-    twitter.update_status(status=tweet)
+        print "All done."
+        csvNumber += 1
